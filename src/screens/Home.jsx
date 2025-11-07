c397187d215e3c6d6a1a067b4b02553f9db28894
Kush Agarwal <kush.301104@gmail.com>
2025-11-07 22:41:05 +0530

    Beautified Home Page

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,7 +1,20 @@
+import { Col, Container, Row } from "react-bootstrap";
+import PublicNavbar from "../components/PublicNavbar";
 import useDynamicTitle from "../hooks/useDynamicTitle";
 
 const Home = () => {
   useDynamicTitle("Home | BootBlog");
-  return <div>Home Page</div>;
-}
+  return (
+    <>
+      <PublicNavbar />
+      <Container className="mt-4">
+        <Row>
+          <Col>
+            <h2>Home Page</h2>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
 export default Home;
