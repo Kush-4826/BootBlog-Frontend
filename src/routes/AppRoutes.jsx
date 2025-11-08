117e15e837863cb8e2ca7551d3f99f73a20063d6
Kush Agarwal <kush.301104@gmail.com>
2025-11-08 18:32:22 +0530

    Added New Routes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -5,6 +5,9 @@ import Register from "../screens/auth/Register";
 import Home from "../screens/Home";
 import Profile from "../screens/Profile";
 import ProtectedRoute from "../components/ProtectedRoute";
+import NotFound from "../screens/errors/NotFound";
+import ProtectedAdminRoutes from "../components/ProtectedAdminRoutes";
+import Dashboard from "../admin/screens/Dashboard";
 
 const AppRoutes = () => {
   const { isAuthenticated } = useAuth();
@@ -15,16 +18,17 @@ const AppRoutes = () => {
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/" element={<Home />} />
+      <Route path="/404" element={<NotFound />} />
 
       {/* Protected Routes */}
-      <Route
-        path="/profile"
-        element={
-          <ProtectedRoute>
-            <Profile />
-          </ProtectedRoute>
-        }
-      />
+      <Route element={<ProtectedRoute />}>
+        <Route path="/profile" element={<Profile />} />
+      </Route>
+
+      <Route element={<ProtectedAdminRoutes />}>
+        <Route path="/admin/dashboard" element={<Dashboard />} />
+      </Route>
+
     </Routes>
   );
 }
