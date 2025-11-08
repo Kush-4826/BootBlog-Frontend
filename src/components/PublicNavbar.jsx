aeb440fb8355b4fc22aa3a6e2b38bebee70b79a6
Kush Agarwal <kush.301104@gmail.com>
2025-11-08 12:37:26 +0530

    Changed the Navbar

--- src/components/PublicNavbar.jsx
+++ src/components/PublicNavbar.jsx
@@ -1,47 +1,67 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import { useAuth } from '../context/AuthContext';
-import { Link, useNavigate } from 'react-router';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { useAuth } from "../context/AuthContext";
+import { Link, useNavigate } from "react-router";
+import { PersonCircle } from "react-bootstrap-icons";
 
 const PublicNavbar = () => {
-  
-  const {isAuthenticated} = useAuth();
+  const { isAuthenticated, logout } = useAuth();
   const navigate = useNavigate();
 
   const navigateToRoute = (route) => {
     navigate(route);
-  }
+  };
+
+  const handleLogout = () => {
+    logout();
+    navigateToRoute("/login");
+  };
 
   return (
-    <Navbar collapseOnSelect expand="lg" className="bg-body-secondary" bg='info' variant='light'>
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      className="bg-body-secondary"
+      bg="info"
+      variant="light"
+    >
       <Container>
-        <Navbar.Brand href="#home">BootBlog</Navbar.Brand>
+        <Navbar.Brand onClick={() => navigateToRoute("/")}>BootBlog</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#features">Features</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
           </Nav>
           <Nav>
             {isAuthenticated ? (
-              <Nav.Link onClick={() => navigateToRoute("/profile")}>Profile</Nav.Link>
+              <NavDropdown
+                title={<PersonCircle width={25} height={25} />}
+                id="collapsible-nav-dropdown"
+              >
+                <NavDropdown.Item onClick={() => navigateToRoute("/profile")}>Profile</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  My Blogs
+                </NavDropdown.Item>
+                {/* <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item> */}
+                <NavDropdown.Divider />
+                <NavDropdown.Item onClick={handleLogout}>
+                  Log Out
+                </NavDropdown.Item>
+              </NavDropdown>
             ) : (
+              // <Nav.Link onClick={() => navigateToRoute("/profile")}>Profile</Nav.Link>
               <>
-              <Nav.Link onClick={() => navigateToRoute("/login")}>Login</Nav.Link>
-              <Nav.Link onClick={() => navigateToRoute("/register")}>Register</Nav.Link>
+                <Nav.Link onClick={() => navigateToRoute("/login")}>
+                  Login
+                </Nav.Link>
+                <Nav.Link onClick={() => navigateToRoute("/register")}>
+                  Register
+                </Nav.Link>
               </>
             )}
           </Nav>
