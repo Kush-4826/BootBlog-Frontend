69adadbc7bf591a087c38a83c56fe7a413f09fe6
Kush Agarwal <kush.301104@gmail.com>
2025-11-08 18:30:35 +0530

    Added Outlet for Multiple Routes

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,7 +1,7 @@
-import { Navigate, useLocation } from "react-router";
+import { Navigate, Outlet, useLocation } from "react-router";
 import { useAuth } from "../context/AuthContext"
 
-const ProtectedRoute = ({children}) => {
+const ProtectedRoute = () => {
   const {isAuthenticated, loading} = useAuth();
   const location = useLocation();
 
@@ -13,7 +13,7 @@ const ProtectedRoute = ({children}) => {
     return <Navigate to="/login" state={{from: location}} replace />
   }
 
-  return children;
+  return <Outlet />;
 }
 
 export default ProtectedRoute;
