92eb013910e80216d8f123b24af32aa6a609156b
Kush Agarwal <kush.301104@gmail.com>
2025-11-10 11:59:17 +0530

    Added The AdminNavbar to the Dashboard

--- src/admin/screens/Dashboard.jsx
+++ src/admin/screens/Dashboard.jsx
@@ -1,5 +1,15 @@
-const Dashboard = () => { 
-  return <div>Dashboard</div>;
+import { Container } from "react-bootstrap";
+import AdminNavbar from "../components/AdminNavbar";
+
+const Dashboard = () => {
+  return (
+    <>
+      <AdminNavbar />
+      <Container fluid={true}>
+        <div>Dashboard</div>
+      </Container>
+    </>
+  );
 };
 
-export default Dashboard; 
+export default Dashboard;
